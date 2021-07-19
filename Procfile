ac1a97e257d48e1a8992ebd9259a04b76a2c11cf
Chaoyuuu <chaoyu2330@gmail.com>
2021-07-20 00:35:47 +0800

    :sparkles: echo version

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: java -jar target/ChaoyuLineBot-1.0-SNAPSHOT.jar
---
+++ src/main/java/tw/chaoyu/MyApplication.java
@@ -0,0 +1,34 @@
+package tw.chaoyu;
+
+import com.linecorp.bot.model.event.Event;
+import com.linecorp.bot.model.event.MessageEvent;
+import com.linecorp.bot.model.event.message.TextMessageContent;
+import com.linecorp.bot.model.message.Message;
+import com.linecorp.bot.model.message.TextMessage;
+import com.linecorp.bot.spring.boot.annotation.EventMapping;
+import com.linecorp.bot.spring.boot.annotation.LineMessageHandler;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author chaoyulee chaoyu2330@gmail.com
+ */
+@SpringBootApplication
+@LineMessageHandler
+public class MyApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MyApplication.class, args);
+    }
+
+    @EventMapping
+    public Message handleTextMessageEvent(MessageEvent<TextMessageContent> event) {
+        System.out.println("event: " + event);
+        final String originalMessageText = event.getMessage().getText();
+        return new TextMessage(originalMessageText);
+    }
+
+    @EventMapping
+    public void handleDefaultMessageEvent(Event event) {
+        System.out.println("event: " + event);
+    }
+}
